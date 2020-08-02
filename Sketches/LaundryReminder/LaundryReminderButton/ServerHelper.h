c7f95062c5075156e0538cd275b69fc99fe1e431
Christian Caicedo <ccaicedo@credera.com>
2020-08-02 14:41:50 -0500

    removed unnecessary code

--- Sketches/LaundryReminder/LaundryReminderButton/ServerHelper.h
+++ Sketches/LaundryReminder/LaundryReminderButton/ServerHelper.h
@@ -2,12 +2,13 @@
 #define ServerHelper_h
 
 #include <Arduino.h>
+#include <WiFi.h>
 
 class ServerHelper
 {
   public:
-    ServerHelper(int pin);
-    void test();
+    ServerHelper();
+    void handleNewClient(WiFiClient client, String Header);
   private:
     int _pin;
 };
