9592259ceb28c2857210a34760111b89d6f087e1
Christian Caicedo <ccaicedo@credera.com>
2020-08-08 17:08:01 -0500

    working on eepromhelper

---
+++ Sketches/LaundryReminder/LaundryReminderButton/EEPROMHelper.cpp
@@ -0,0 +1,5 @@
+#include "EEPROMHelper.h"
+
+EEPROMHelper::EEPROMHelper()
+{
+}
---
+++ Sketches/LaundryReminder/LaundryReminderButton/EEPROMHelper.h
@@ -0,0 +1,13 @@
+#ifndef EEPROMHelper_h
+#define EEPROMHelper_h
+
+class EEPROMHelper
+{
+  public:
+    EEPROMHelper();
+  private:
+    int _ssidLocation;
+    int _pwLocation;
+};
+
+#endif
